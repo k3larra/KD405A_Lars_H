7e444b54043624f008b4220091ba0431123f7827
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2016-02-27 11:08:06 +0100

    Assignment 7 parts

---
+++ A7_Lars_H/src/TryIt.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import se.mah.k3.klara.PixelController;
+import se.mah.k3.klara.PixelController.Screen;
+
+public class TryIt extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TryIt frame = new TryIt();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TryIt() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		PixelController px = PixelController.getInstance(Screen.ASSIGNMENT_7);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		px.setPixel(10, 10, Color.WHITE);
+	}
+
+}
